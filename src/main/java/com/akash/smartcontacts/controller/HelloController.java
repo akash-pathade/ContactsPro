7a15fb709990abea6eb6f3e052fcc5d76cdae2e6
Akash Pathade <pathadeji@gmail.com>
2021-02-19 22:38:41 +0530

    Added bootstrap theme and set up home page

--- src/main/java/com/akash/smartcontacts/controller/HelloController.java
+++ src/main/java/com/akash/smartcontacts/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.akash.smartcontacts.controller;
 import com.akash.smartcontacts.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,22 +13,32 @@ public class HelloController {
     private UserRepository userRepository;
 
     @GetMapping("home")
-    public String home(){
-
-//        User user = new User();
-//        user.setName("Akash");
-//        user.setAbout("Hi this is akash");
-//        userRepository.save(user);
-        return "home";
+    public String home(Model model) {
+        model.addAttribute("title", "Smart Contact Manager | Home");
+        return "index";
     }
 
     @GetMapping("about")
-    public String about(){
-
-//        User user = new User();
-//        user.setName("Akash");
-//        user.setAbout("Hi this is akash");
-//        userRepository.save(user);
+    public String about(Model model) {
+        model.addAttribute("title", "Smart Contact Manager | About");
         return "about";
+    }
+
+    @GetMapping("index")
+    public String index(Model model) {
+        model.addAttribute("title", "Smart Contact Manager | Home");
+        return "index";
+    }
+
+    @GetMapping("login")
+    public String login(Model model) {
+        model.addAttribute("title", "Smart Contact Manager | login");
+        return "login";
+    }
+
+    @GetMapping("signup")
+    public String signup(Model model) {
+        model.addAttribute("title", "Smart Contact Manager | signup");
+        return "signup";
     }
 }
